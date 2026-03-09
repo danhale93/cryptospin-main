822d2a792e4bbbdf7ba7ded140037b606994177e
danhale93 <danhale93@gmail.com>
2026-03-09 04:27:47 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8082',
+        target: 'http://localhost:8081',
         changeOrigin: true,
       }
     }
